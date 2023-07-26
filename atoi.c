1a8e323d03f0d6bda363de6555031ba85469976b
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-26 19:47:21 +0300

    simple shell

---
+++ atoi.c
@@ -0,0 +1,36 @@
+#include "shell_library.h"
+
+/**
+ *_atoi - it converts a string into an integer
+ *@str: the string that will be converted
+ *
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *str)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  str[i] != '\0' && flag != 2; i++)
+	{
+		if (str[i] == '-')
+			sign *= -1;
+
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (str[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
